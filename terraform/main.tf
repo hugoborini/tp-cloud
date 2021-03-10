9c7f5ce7d2f711bc5ebd3d19a44e114a94774467
hugoborini <hugo.borini@hetic.net>
2021-03-10 21:50:21 +0100

    {add} terraform

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+terraform{
+    required_providers {
+      heroku = {
+          source= "heroku/heroku"
+          version= "3.2.0"
+      }
+    }
+}
+
+provider "heroku"{
+    email = var.email_address
+    api_key = var.heroku_api_key
+}
+
+resource "heroku_app" "tp_cloud_app_prod" {
+  name   = "tp-cloud-app"
+  region = "us"
+}
+
+resource "heroku_build" "tp_cloud_app_prod" {
+  app = heroku_app.tp_cloud_app_prod.id
+
+  source = {
+    path = "../app"
+  }
+  provisioner "local-exec" {
+      command = "bash ./scripts/testRes.sh ${heroku_app.tp_cloud_app_prod.web_url}"
+  }
+}
+
+output "web_url" {
+  value = heroku_app.tp_cloud_app_prod.web_url 
+}
